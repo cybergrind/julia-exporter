0c2828970396eb21634961368263ffe6c284202a
Kirill Pinchuk <cybergrind@gmail.com>
2022-07-13 13:42:59 +0300

    basic http server

--- src/JuliaExporter.jl
+++ src/JuliaExporter.jl
@@ -1,5 +1,25 @@
 module JuliaExporter
+using HTTP
 
 greet() = print("Hello World!")
+
+function startServer()
+    # HTTP.listen! and HTTP.serve! are the non-blocking versions of HTTP.listen/HTTP.serve
+	server = HTTP.serve!("127.0.0.1", 8088) do request::HTTP.Request
+	   @show request
+	   @show request.method
+	   @show HTTP.header(request, "Content-Type")
+	   @show request.body
+	   try
+	       return HTTP.Response("Hello")
+	   catch e
+	       return HTTP.Response(400, "Error: $e")
+	   end
+	end
+end
+
+greet()
+#server = startServer()
+#wait(server)
 
 end # module
