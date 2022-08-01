13a62897d924e46275af0ae08e16b0f034b9128a
Kirill Pinchuk <cybergrind@gmail.com>
2022-08-01 23:56:34 +0300

    repl: development setup

--- run.jl
+++ run.jl
@@ -1,8 +1,12 @@
 import Pkg
 Pkg.activate(".")
 
+# using Revise  # hot-reload
+
 using JuliaExporter
 
 Pkg.precompile()
+
+# JuliaExporter.startServer()
 
 wait(JuliaExporter.startServer())
--- src/JuliaExporter.jl
+++ src/JuliaExporter.jl
@@ -10,6 +10,23 @@ struct Metric
     value::Union{Int,Float64}
 end
 
+struct Storage
+    now::Metric
+    prev::Metric
+    prev_prev::Metric
+end
+
+storage = Storage(
+    Metric("now", [], 0),
+    Metric("prev", [], 0),
+    Metric("prev_prev", [], 0)
+)
+
+function updateMetric()
+    # getfield(storage, :prev_prev)
+end
+
+
 function startServer(host="127.0.0.1", port=8088)
     # HTTP.listen! and HTTP.serve! are the non-blocking versions of HTTP.listen/HTTP.serve
     @printf("Start server on port %s:%d\n", host, port)
